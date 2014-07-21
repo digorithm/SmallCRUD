ccb05b16404742ec357367e948863261a62cf22d
digorithm <contatodigo@hotmail.com>
2014-07-21 13:56:30 -0300

    First commit.

---
+++ src/java/dao/testador.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.sql.SQLException;
+
+public class testador {
+	public testador(){
+		
+	}
+	  public static void main(String [] args) throws SQLException{
+		
+		PersonDAO p = new PersonDAO();
+                                    System.out.println("ei");
+	}
+}
